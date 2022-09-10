1f652be43a3229ea61f5b6d09229dc62a73336ed
Aperryhu <aperryhu@chicagobooth.edu>
2022-09-10 23:56:04 +0000

    Own Photos

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,19 @@ class UsersController < ApplicationController
   end
 
   def show
+    #Parameters: {"path_username" => "anisa"}
+    url_username = params.fetch("path_username")
+
+    matching_usernames = User.where({:username => url_username})
+
+    @the_user = matching_usernames.at(0)
+
+    #if @the_user == nil
+   #     redirect_to ("/404")
+    
     render({:template => "user_templates/show.html.erb"})
+    #end
+
   end
 
 end
